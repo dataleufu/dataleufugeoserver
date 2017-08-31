14fe302e7679560b847f84e1239753f4a1ceaf3e
milena <milena.armada@radiocut.fm>
2017-08-31 20:59:19 -0300

    Corrección parámetros de archivos estáticos

--- dataleufu/settings.py
+++ dataleufu/settings.py
@@ -168,6 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../local_static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
